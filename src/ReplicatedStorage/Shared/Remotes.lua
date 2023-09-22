5848a33b0afbd385d4935a61b8ebfc55e1626115
Magik Manz <magikaman12@gmail.com>
2023-09-22 21:11:39 +0300

    Make remotes module

---
+++ src/ReplicatedStorage/Shared/Remotes.lua
@@ -0,0 +1,8 @@
+local ReplicatedStorage = game:GetService("ReplicatedStorage")
+
+local Net = require(ReplicatedStorage.Packages.Net)
+local t = require(ReplicatedStorage.Packages.t)
+
+return Net.CreateDefinitions({
+    
+})
