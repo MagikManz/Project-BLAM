341e89fa3887d76c26977d6388da1c6e529e9d39
Magik Manz <magikaman12@gmail.com>
2023-10-05 04:12:14 +0300

    Start setting up inputs handler

---
+++ src/StarterPlayerScripts/InputHandler.lua
@@ -0,0 +1,16 @@
+local ReplicatedStorage = game:GetService("ReplicatedStorage")
+local UserInputService = game:GetService("UserInputService")
+
+local Inputs = require(ReplicatedStorage.Client.Inputs)
+
+UserInputService.InputBegan:Connect(function()
+
+end)
+
+UserInputService.InputChanged:Connect(function()
+
+end)
+
+UserInputService.InputEnded:Connect(function()
+
+end)
